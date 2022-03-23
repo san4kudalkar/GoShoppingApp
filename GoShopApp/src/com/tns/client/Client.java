28af94f520017b1695f2812fcdd9f413207a6102
Sanik Kudalkar <Sanik Kudalkar@Sanik>
2022-03-23 16:52:03 +0530

    First Commit

---
+++ GoShopApp/src/com/tns/client/Client.java
@@ -0,0 +1,46 @@
+package com.tns.client;
+
+import com.tns.application.GSNormalAcc;
+import com.tns.application.GSPrimeAcc;
+import com.tns.application.GSShopFactory;
+
+
+
+public class Client 
+{
+	public static void main(String[] args) 
+	{
+		// Step 1 - Step a
+		GSShopFactory gssfactory = new GSShopFactory();
+		gssfactory.getNewPrimeAccount(123, "Sanik", 800, true);
+		gssfactory.getNewNormalAccount(111, "Rohit", 800, 50);
+		
+		
+		// Step 2
+		GSPrimeAcc gsprime = new GSPrimeAcc();
+		gsprime.getAccNm();
+		gsprime.getAccNo();
+		gsprime.getCharges();
+		// Step 4 / Step d.
+		gsprime.bookProduct(800);
+		
+		// Step 5 / Step e.
+		gsprime.toString();
+		
+		// Step 3
+		GSNormalAcc gsnormal = new GSNormalAcc(); 
+		gsnormal.getAccNm();
+		gsnormal.getAccNo();
+		gsnormal.getCharges();
+		gsnormal.getDeliveryCharge();
+		
+		// Step 4 / Step d.
+		gsnormal.bookProduct(800);
+		// Step 5 / Step e.
+		gsnormal.toString();
+	
+		
+
+	}
+
+}
